f9819b401e4ea6ef10fb733231e304e1507bcac6
Jumman04 <arsharifuddinjumman04@gmail.com>
2023-11-09 11:10:55 +0600

    Updated JSlider.kt

--- J-Slider/src/main/java/com/jummania/j_slider/JSlider.kt
+++ J-Slider/src/main/java/com/jummania/j_slider/JSlider.kt
@@ -2,7 +2,11 @@ package com.jummania.j_slider
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -10,8 +14,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.Scroller
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.jummania.j_slider.JSlider.Slider.BooleanObject.autoSlidingBoolean
+import com.jummania.j_slider.JSlider.Slider.BooleanObject.indicatorBoolean
+import com.jummania.j_slider.JSlider.Slider.BooleanObject.isDragging
+import com.jummania.j_slider.JSlider.Slider.IntObject.defaultIndicatorColor
+import com.jummania.j_slider.JSlider.Slider.IntObject.selectedIndicatorColor
+import com.jummania.j_slider.JSlider.Slider.IntObject.size
+import com.jummania.j_slider.JSlider.Slider.IntObject.slidingDuration
 import com.jummania.j_slider.animations.AnimationTypes
 import com.jummania.j_slider.animations.BackgroundToForeground
 import com.jummania.j_slider.animations.CubeIn
@@ -29,6 +41,7 @@ import com.jummania.j_slider.animations.TabletSlide
 import com.jummania.j_slider.animations.Toss
 import com.jummania.j_slider.animations.ZoomIn
 import com.jummania.j_slider.animations.ZoomOut
+import kotlin.math.min
 
 /**
  * Created by Jummania on 08,November,2023.
@@ -40,6 +53,11 @@ class JSlider @JvmOverloads constructor(
     context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
 
+    /**
+     * Created by Jummania on 08,November,2023.
+     * Email: sharifuddinjumman@gmail.com
+     * Dhaka, Bangladesh.
+     */
 
     private val jSlider: Slider by lazy {
         Slider(context)
@@ -101,6 +119,12 @@ class JSlider @JvmOverloads constructor(
     }
 
     abstract class Slide : PagerAdapter() {
+
+        /**
+         * Created by Jummania on 08,November,2023.
+         * Email: sharifuddinjumman@gmail.com
+         * Dhaka, Bangladesh.
+         */
         abstract fun getView(layoutInflater: LayoutInflater, parent: ViewGroup): View
         abstract fun onSliderCreate(view: View, position: Int)
         override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
@@ -207,5 +231,176 @@ class JSlider @JvmOverloads constructor(
 
     private fun exception() {
         if (jSlider.adapter != null) throw IllegalArgumentException("You cannot change anything on the Slider after setSlider() method call.")
+    }
+
+    private class Slider(context: Context) : ViewPager(context) {
+
+        /**
+         * Created by Jummania on 08,November,2023.
+         * Email: sharifuddinjumman@gmail.com
+         * Dhaka, Bangladesh.
+         */
+
+        val updateHandler by lazy {
+            Handler(Looper.getMainLooper())
+        }
+        private lateinit var update: Runnable
+
+        private object IntObject {
+            var slidingDuration: Long = 1555
+            var size = 30
+            var selectedIndicatorColor = Color.WHITE
+            var defaultIndicatorColor = Color.parseColor("#80ffffff")
+        }
+
+        private object BooleanObject {
+            var isDragging: Boolean = false
+            var indicatorBoolean = true
+            var autoSlidingBoolean = true
+        }
+
+
+        fun setSlider(slider: Slide, dotIndicatorLayout: LinearLayout) {
+            if (slider.count > 0) {
+                super.setAdapter(slider)
+
+                val sliders = slider.count
+                val dots: MutableList<JLayout> by lazy { mutableListOf() }
+
+                if (indicatorBoolean) {
+                    val dotLayoutParams = LinearLayout.LayoutParams(size, size)
+                    for (i in 0 until sliders) {
+                        val dot = JLayout(context)
+                        dotLayoutParams.marginStart = 6
+                        dotLayoutParams.marginEnd = 6
+                        dot.layoutParams = dotLayoutParams
+                        dot.setBackgroundResource(R.drawable.indicator)
+                        dot.setColor(defaultIndicatorColor)
+
+                        dotIndicatorLayout.addView(dot)
+                        dots.add(dot)
+                    }
+                    if (dots.isNotEmpty()) dots[0].setColor(selectedIndicatorColor)
+
+                }
+
+                update = Runnable {
+                    if (!isDragging && autoSlidingBoolean) {
+                        var currentPage: Int = currentItem
+                        if (currentPage == sliders - 1) currentPage = 0
+                        else currentPage += 1
+                        setCurrentItem(currentPage, true)
+                    }
+                }
+
+                addOnPageChangeListener(object : OnPageChangeListener {
+                    override fun onPageScrolled(
+                        position: Int, positionOffset: Float, positionOffsetPixels: Int
+                    ) {
+
+                    }
+
+                    override fun onPageSelected(position: Int) {
+                        if (indicatorBoolean && dots.isNotEmpty()) {
+
+                            for (dot in dots) dot.setColor(defaultIndicatorColor)
+                            dots[position].setColor(selectedIndicatorColor)
+                        }
+                        if (autoSlidingBoolean) {
+                            updateHandler.removeCallbacks(update)
+                            updateHandler.postDelayed(update, slidingDuration)
+                        }
+                    }
+
+                    override fun onPageScrollStateChanged(state: Int) {
+                        if (isDragging && state != SCROLL_STATE_DRAGGING && autoSlidingBoolean) {
+                            updateHandler.removeCallbacks(update)
+                            updateHandler.postDelayed(update, slidingDuration)
+                        }
+                        isDragging = state == SCROLL_STATE_DRAGGING
+                    }
+                })
+
+                try {
+                    val viewPagerScroller = ViewPager::class.java.getDeclaredField("mScroller")
+                    viewPagerScroller.isAccessible = true
+                    viewPagerScroller[this] = MyScroller(context)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+
+        private inner class MyScroller(context: Context?) : Scroller(context) {
+
+            /**
+             * Created by Jummania on 08,November,2023.
+             * Email: sharifuddinjumman@gmail.com
+             * Dhaka, Bangladesh.
+             */
+
+            override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+                super.startScroll(startX, startY, dx, dy, slidingDuration.toInt())
+            }
+        }
+
+        fun setSlidingDuration(duration: Long) {
+            slidingDuration = duration
+        }
+
+        fun setIndicatorSize(indicatorSize: Int) {
+            size = indicatorSize
+        }
+
+        fun setIndicatorColor(defaultColor: Int, selectedColor: Int) {
+            defaultIndicatorColor = defaultColor
+            selectedIndicatorColor = selectedColor
+        }
+
+        fun enableIndicator(boolean: Boolean) {
+            indicatorBoolean = boolean
+        }
+
+        fun enableAutoSliding(boolean: Boolean) {
+            autoSlidingBoolean = boolean
+        }
+
+        override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
+            if (hasWindowFocus && autoSlidingBoolean) {
+                updateHandler.removeCallbacks(update)
+                updateHandler.postDelayed(update, slidingDuration)
+            } else updateHandler.removeCallbacks(update)
+            super.onWindowFocusChanged(hasWindowFocus)
+        }
+
+        private class JLayout(context: Context?) : LinearLayout(context) {
+
+            /**
+             * Created by Jummania on 08,November,2023.
+             * Email: sharifuddinjumman@gmail.com
+             * Dhaka, Bangladesh.
+             */
+
+            private val paint = Paint()
+
+            init {
+                paint.style = Paint.Style.FILL
+            }
+
+            override fun onDraw(canvas: Canvas) {
+                canvas.drawCircle(
+                    (width / 2).toFloat(),
+                    height.toFloat() / 2,
+                    (min(width, height) / 2).toFloat(),
+                    paint
+                )
+            }
+
+            fun setColor(color: Int) {
+                paint.color = color
+                invalidate() // Request a redraw to reflect the changes
+            }
+        }
     }
 }
--- app/src/main/java/com/jummania/jummaniaslider/MainActivity.kt
+++ app/src/main/java/com/jummania/jummaniaslider/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.jummania.jummaniaslider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.jummania.j_slider.JSlider
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val jSlider:JSlider = findViewById(R.id.jSlider)
+        val jSlider: JSlider = findViewById(R.id.jSlider)
+
+        createList()
         jSlider.setSlider(Slider())
+
     }
 
-    private inner class Slider: JSlider.Slide() {
+    private inner class Slider : JSlider.Slide() {
         override fun getView(layoutInflater: LayoutInflater, parent: ViewGroup): View {
             return layoutInflater.inflate(R.layout.activity_main, parent, false)
         }
@@ -32,17 +35,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun createList(){
-        add("Sharif Uddin Jumman", "https://jummania.com/App/BanglaNatokSamahar/Images/Cover%20Photo.jpg")
+    private fun createList() {
+        add(
+            "Sharif Uddin Jumman",
+            "https://jummania.com/App/BanglaNatokSamahar/Images/Cover%20Photo.jpg"
+        )
         add("Anindya Das", "https://jummania.com/App/BanglaNatokSamahar/Images/Aninda das.jpg")
         add("MD Abdullah", "https://avatars.githubusercontent.com/u/110069678?v=4")
-        add("MD Sarowar Hosain Shuvo",
+        add(
+            "MD Sarowar Hosain Shuvo",
             "https://scontent.fdac142-1.fna.fbcdn.net/v/t1.6435-9/120902465_785023465615624_7635398377734730401_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=be3454&_nc_ohc=azvmlXXjIDEAX8eil0Z&_nc_ht=scontent.fdac142-1.fna&oh=00_AfCRLl3dlz8aqhEmHWqEUtaUcvq65g2gdx67Nlr0IsTttA&oe=6550B2AD"
         )
-        add("Sharif Uddin Jumman", "https://jummania.com/App/BanglaNatokSamahar/Images/Profile%20Photo.jpg")
+        add(
+            "Sharif Uddin Jumman",
+            "https://jummania.com/App/BanglaNatokSamahar/Images/Profile%20Photo.jpg"
+        )
     }
 
-    private fun add(name:String, photo:String){
+    private fun add(name: String, photo: String) {
         arrayList.add(DataModel(name, photo))
     }
 }
