117bda9afdfe54561b5ecdeca99a512d5e7a6a46
CoderBDK <cobdgithub07@gmail.com>
2023-11-23 17:37:51 +0000

    Major issue fixed, now the code is supported in Java

--- J-Slider/src/main/java/com/jummania/widgets/JIndicator.kt
+++ J-Slider/src/main/java/com/jummania/widgets/JIndicator.kt
@@ -6,7 +6,6 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.widget.LinearLayout
 import com.jummania.types.ShapeTypes
-import kotlin.math.min
 
 /**
  * Abstract class representing a custom layout with a colored shape indicator.
@@ -31,7 +30,7 @@ internal abstract class JIndicator(
     }
 
     // Path object used for drawing shapes
-    private val path by lazy { Path() }
+    // private val path by lazy { Path() }
 
     /**
      * Override the onDraw method to draw a colored shape in the center of the layout.
@@ -41,100 +40,7 @@ internal abstract class JIndicator(
     override fun onDraw(canvas: Canvas) {
         // Draw the specified shape indicator with the given color
         // Draw a colored circle in the center of the layout
-        when (shapeTypes) {
-            ShapeTypes.CIRCLE -> {
-                canvas.drawCircle(
-                    (width / 2f), height / 2f, (min(width, height) / 2f), paint
-                )
-            }
-
-            ShapeTypes.HEART -> {
-                // Starting point
-                path.moveTo(width / 2f, height / 5f)
-
-                // Upper left path
-                path.cubicTo(
-                    5 * width / 14f, 0f, 0f, height / 15f, width / 28f, 2 * height / 5f
-                )
-
-                // Lower left path
-                path.cubicTo(
-                    width / 14f,
-                    2 * height / 3f,
-                    3 * width / 7f,
-                    5 * height / 6f,
-                    width / 2f,
-                    height.toFloat()
-                )
-
-                // Lower right path
-                path.cubicTo(
-                    4 * width / 7f,
-                    5 * height / 6f,
-                    13 * width / 14f,
-                    2 * height / 3f,
-                    27 * width / 28f,
-                    2 * height / 5f
-                )
-
-                // Upper right path
-                path.cubicTo(
-                    width.toFloat(), height / 15f, 9 * width / 14f, 0f, width / 2f, height / 5f
-                )
-
-                canvas.drawPath(path, paint)
-            }
-
-            ShapeTypes.SQUARE -> {
-                canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
-            }
-
-            ShapeTypes.STAR -> {
-                paint.isAntiAlias = true
-                paint.style = Paint.Style.STROKE
-
-                var mid = (width / 2).toFloat()
-                val min = min(width, height).toFloat()
-                val fat = min / 17
-                val half = min / 2
-                val rad = half - fat
-                mid -= half
-
-                paint.strokeWidth = fat
-                paint.style = Paint.Style.STROKE
-
-                canvas.drawCircle(mid + half, half, rad, paint)
-
-                path.reset()
-
-                paint.style = Paint.Style.FILL
-
-
-                // top left
-
-
-                // top left
-                path.moveTo(mid + half * 0.5f, half * 0.84f)
-                // top right
-                // top right
-                path.lineTo(mid + half * 1.5f, half * 0.84f)
-                // bottom left
-                // bottom left
-                path.lineTo(mid + half * 0.68f, half * 1.45f)
-                // top tip
-                // top tip
-                path.lineTo(mid + half * 1.0f, half * 0.5f)
-                // bottom right
-                // bottom right
-                path.lineTo(mid + half * 1.32f, half * 1.45f)
-                // top left
-                // top left
-                path.lineTo(mid + half * 0.5f, half * 0.84f)
-
-                path.close()
-                canvas.drawPath(path, paint)
-            }
-        }
+        shapeTypes.draw(width, height, paint, canvas)
     }
 
     /**
